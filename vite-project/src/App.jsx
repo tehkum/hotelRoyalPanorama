b436338f0007af5d0679dd275262c99e3ff2cd4a
tehkum kanchwala <tehkumkanchwala@gmail.com>
2023-03-26 17:50:42 +0530

    initial commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,30 @@
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return(<div className="App">
+        <Header />
+        {/* <footer>
+        <ul>
+            <li>Whatsapp</li>
+            <li>Instagram</li>
+            <li>Linkdin</li>
+            <li>Twitter</li>
+            <li>Facebook</li>
+          </ul>
+    
+</footer> */}
+        <h1>this  is for tesstind</h1>
+        <ul>
+        <li>Whatsapp</li>
+            <li>Instagram</li>
+            <li>Linkdin</li>
+            <li>Twitter</li>
+            <li>Facebook</li>
+        </ul>
+          
+      </div>);
+      
+}
+
+export default App;
---
+++ vite-project/src/components/Header.jsx
@@ -0,0 +1,21 @@
+function Header(){
+    return(
+        <div className="App-header">
+        <h2>Hotel Royal Panorama</h2>
+        <span className="Header-bullets">
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Gallery</li>
+            <li>Tariffs and Reservation</li>
+            <li>Facilities</li>
+            <li>Blogs</li>
+            <li>Restaurant</li>
+            <li>Contact Us</li>
+          </ul>
+        </span>
+      </div>
+    );
+}
+
+export default Header;
